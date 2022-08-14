4e70a2afe44bd7c42f8b34db1c9b6224b8b8c624
msmoiz <mustafa.moiz125@gmail.com>
2022-08-13 19:25:23 -0700

    Create project studio.
    The studio includes a development Unreal Engine project that makes it
    easier to simply clone the package and start developing on the plugin.

---
+++ Source/CursoryStudio/CursoryStudio.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CursoryStudio.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CursoryStudio, "CursoryStudio" );
